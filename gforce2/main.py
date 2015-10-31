d934cd5b1e328b49b161088053595697583cedbc
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2015-10-31 17:45:38 -0200

    overall refactoring of the panda3d model rendering (preparing to generalize it to render any MAME 3d model described by a data structure)

--- gforce2/main.py
+++ gforce2/main.py
@@ -21,26 +21,64 @@ import colorsys
 def clamp(i, mn=0, mx=1):
     return min(max(i, mn), mx)
 
-class GalaxyForce(ShowBase):
+gforce2_data = {
+        'id': "gforce2",
+        'name': "Galaxy Force II Super Deluxe"
+}
 
-    def __init__(self):
+class MAMEDevice(ShowBase):
+
+    def __init__(self, device_data):
         # Initialize the ShowBase class from which we inherit, which will
         # create a window and set up everything we need for rendering into it.
         ShowBase.__init__(self)
+        self.device_data = device_data
+        self.setup_scene()
+        self.setup_model()
+        self.setup_event_handlers()
 
+    def setup_model(self):
+        heading = 130
+        self.spaceship = render.attachNewNode("spaceShip")
+        self.spaceship.setPosHpr(0, 60, -10, heading, -90, 0)
+
+        self.static_spaceship = render.attachNewNode("staticSpaceShip")
+        self.static_spaceship.setPosHpr(0, 60, -10, heading, -90, 0)
+
+        self.load_3D_Model(name='glass', color=(0.7, 0.7, 0.7, 0.7), parent=self.spaceship)
+        self.load_3D_Model(name='shinny_metal', color=(0.6, 0.6, 0.6, 1), metalic=True, parent=self.spaceship)
+        bm = self.load_3D_Model(name='dark_black_metal', color=(0.3, 0.3, 0.35, 1), parent=self.spaceship)
+        self.bluePointLight = self.addLight(name="bluePointLight", parent=bm, attenuation=LVector3(.1, 0.04, 0.0), color=(0, 0, .35, 1))
+
+        self.load_3D_Model(name='static_shinny_metal', color=(0.6, 0.6, 0.6, 1), metalic=True, parent=self.static_spaceship)
+        self.load_3D_Model(name='static_golden', color=(0.8, 0.5, 0.2, 1), metalic=True, parent=self.static_spaceship)
+        self.load_3D_Model(name='static_red', color=(0.8, 0, 0, 1), parent=self.static_spaceship)
+
+    def setup_event_handlers(self):
+        # listen to keys for controlling the lights
+        self.accept("escape", sys.exit)
+        self.accept("a", self.toggleLights, [[self.ambientLight]])
+        self.accept("d", self.toggleLights, [[self.directionalLight]])
+        self.accept("p", self.toggleLights, [[self.bluePointLight]])
+        self.accept("l", self.togglePerPixelLighting)
+        self.accept("e", self.toggleShadows)
+        self.accept("z", self.addBrightness, [self.ambientLight, -.05])
+        self.accept("x", self.addBrightness, [self.ambientLight, .05])
+        self.accept("c", self.addBrightness, [self.directionalLight, -.05])
+        self.accept("v", self.addBrightness, [self.directionalLight, .05])
+        self.accept("h", self.rotateShip, [self.spaceship, 5])
+        self.accept("g", self.rotateShip, [self.spaceship, -5])
+
+    def setup_scene(self):
         # The main initialization of our class
         # This creates the on screen title that is in every tutorial
-        self.title = OnscreenText(text="Galaxy Force II Super Deluxe",
+        self.title = OnscreenText(text=self.device_data['name'],
                                   style=1, fg=(1, 1, 0, 1), shadow=(0, 0, 0, 0.5),
                                   pos=(0.87, -0.95), scale = .07)
 
-        self.disco = loader.loadModel("../common/egg/disco_hall")
-        self.disco.reparentTo(render)
-        self.disco.setPosHpr(0, 80, -10, 90, 0, 0)
-        self.disco.setColor((0.5, 0.6, 0.5, 1))
+        self.load_3D_Model(filename = "../common/egg/disco_hall", color=(0.5, 0.6, 0.5, 1), position=(0, 80, -10), hpr=(90, 0, 0), scale=1)
 
-        # First we create an ambient light. All objects are affected by ambient
-        # light equally
+        # First we create an ambient light. All objects are affected by ambient light equally
         # Create and name the ambient light
         self.ambientLight = render.attachNewNode(AmbientLight("ambientLight"))
         # Set the color of the ambient light
@@ -59,92 +97,45 @@ class GalaxyForce(ShowBase):
         dlens = self.directionalLight.node().getLens()
         dlens.setFilmSize(41, 21)
         dlens.setNearFar(50, 75)
-        # self.directionalLight.node().showFrustum()
-        
-        self.glass = loader.loadModel('egg/gforce2_glass')
-        self.glass.setColor((0.7, 0.7, 0.7, 0.7))
-        self.glass.setPos(0, 0, 0)
-        self.glass.setScale(.005)
 
-        self.shinny_metal = loader.loadModel('egg/gforce2_shinny_metal')
-        self.shinny_metal.setColor((0.6, 0.6, 0.6, 1))
-        self.shinny_metal.setPos(0, 0, 0)
-        self.shinny_metal.setScale(.005)
-
-        self.static_shinny_metal = loader.loadModel('egg/gforce2_static_shinny_metal')
-        self.static_shinny_metal.setColor((0.6, 0.6, 0.6, 1))
-        self.static_shinny_metal.setPos(0, 0, 0)
-        self.static_shinny_metal.setScale(.005)
-
-        metalic = Material()
-        metalic.setShininess(20.0)
-        metalic.setSpecular((0.6, 0.6, 0.9, 1))
-        self.shinny_metal.setMaterial(metalic)
-        self.static_shinny_metal.setMaterial(metalic)
-
-        self.black_metal = loader.loadModel('egg/gforce2_dark_black_metal.egg')
-        self.black_metal.setColor((0.3, 0.3, 0.35, 1))
-        self.black_metal.setPos(0, 0, 0)
-        self.black_metal.setScale(.005)
-        
-        self.static_golden = loader.loadModel('egg/gforce2_static_golden.egg')
-        self.static_golden.setColor((0.8, 0.5, 0.2, 1))
-        self.static_golden.setPos(0, 0, 0)
-        self.static_golden.setScale(.005)
-        self.static_golden.setMaterial(metalic)
-
-        self.static_red = loader.loadModel('egg/gforce2_static_red.egg')
-        self.static_red.setColor((0.8, 0, 0, 1))
-        self.static_red.setPos(0, 0, 0)
-        self.static_red.setScale(.005)
-
-        # The blue point light
-        # Point lights are lights that radiate from a single point, like a light bulb.
-        # Like spotlights, they are given position by attaching them to NodePaths in the world
-        self.bluePointLight = self.black_metal.attachNewNode(
-            PointLight("bluePointLight"))
-        self.bluePointLight.node().setAttenuation(LVector3(.1, 0.04, 0.0))
-        self.bluePointLight.node().setColor((0, 0, .35, 1))
-        self.bluePointLight.node().setSpecularColor((1, 1, 1, 1))
-
-        heading = 130
-        self.spaceship = render.attachNewNode("spaceShip")
-        self.spaceship.setPosHpr(0, 60, -10, heading, -90, 0)
-
-        self.static_spaceship = render.attachNewNode("staticSpaceShip")
-        self.static_spaceship.setPosHpr(0, 60, -10, heading, -90, 0)
-
-        self.glass.reparentTo(self.spaceship)
-        self.shinny_metal.reparentTo(self.spaceship)
-        self.black_metal.reparentTo(self.spaceship)
-
-        self.static_shinny_metal.reparentTo(self.static_spaceship)
-        self.static_golden.reparentTo(self.static_spaceship)
-        self.static_red.reparentTo(self.static_spaceship)
-        
         # Finally we store the light on the root of the scene graph.
         # This will cause them to affect everything in the scene.
         render.setLight(self.ambientLight)
         render.setLight(self.directionalLight)
-        render.setLight(self.bluePointLight)
 
         # Per-pixel lighting and shadows are initially off
         self.perPixelEnabled = False
         self.shadowsEnabled = False
 
-        # listen to keys for controlling the lights
-        self.accept("escape", sys.exit)
-        self.accept("a", self.toggleLights, [[self.ambientLight]])
-        self.accept("d", self.toggleLights, [[self.directionalLight]])
-        self.accept("p", self.toggleLights, [[self.bluePointLight]])
-        self.accept("l", self.togglePerPixelLighting)
-        self.accept("e", self.toggleShadows)
-        self.accept("z", self.addBrightness, [self.ambientLight, -.05])
-        self.accept("x", self.addBrightness, [self.ambientLight, .05])
-        self.accept("c", self.addBrightness, [self.directionalLight, -.05])
-        self.accept("v", self.addBrightness, [self.directionalLight, .05])
-        self.accept("h", self.rotateShip, [self.spaceship, 5])
-        self.accept("g", self.rotateShip, [self.spaceship, -5])
+    def load_3D_Model(self, filename=None, name=None, parent=None, color=(1,1,1,1), position=(0,0,0), hpr=(0,0,0),
+                      scale=0.005, metalic=False, shininess=20, specular=(0.6, 0.6, 0.9, 1)):
+        if parent is None:
+            parent = render
+
+        if name:
+            filename = 'egg/%s_%s' % (self.device_data['id'], name)
+
+        model = loader.loadModel(filename)
+        model.setColor(color)
+        model.setPosHpr(position[0], position[1], position[2], hpr[0], hpr[1], hpr[2])
+        model.setScale(scale)
+
+        if metalic:
+            m = Material()
+            m.setShininess(shininess)
+            m.setSpecular(specular)
+            model.setMaterial(m)
+
+        model.reparentTo(parent)
+        return model
+
+    def addLight(self, name, parent, attenuation=LVector3(.1, 0.04, 0.0), color=(1, 1, 1, 1), specular=(1, 1, 1, 1)):
+        light = parent.attachNewNode(PointLight(name))
+        light.node().setAttenuation(attenuation)
+        light.node().setColor(color)
+        light.node().setSpecularColor(specular)
+        render.setLight(light)
+        return light
 
     def rotateShip(self, part, angle):
         part.setH(angle + part.getH())
@@ -204,9 +195,7 @@ class GalaxyForce(ShowBase):
 
 
 # Make an instance of our class and run the demo
-demo = GalaxyForce()
-#demo.run()
-
+demo = MAMEDevice(gforce2_data)
 
 fifo = open("/tmp/sdlmame_out")
 states = {'start_lamp': '0'}
